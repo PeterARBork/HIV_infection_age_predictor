c1d6ddbff3b9f74733e262d77a9fc68e4b2be222
Peter Bork <PeterARBork@gmail.com>
2017-12-27 14:31:20 +0100

    Added JC69 prediction.

--- infection_time.py
+++ infection_time.py
@@ -1,4 +1,4 @@
-""" Hammings predicts age of HIV infection from pileup files.
+""" Predicts age of HIV infection from pileup files.
 
 All files in same folder as this executable with pileup filenames
 will be used as input if their names have the following structure:
@@ -19,10 +19,10 @@ http://www.biorxiv.org/content/early/2017/04/21/129387
 Usage example:
 Place this executable in a folder with only .mpileup files to be
 calculated on and make sure the file is executable (i.e. use
-> chmod +x hammings.py
+> chmod +x infection_time.py
 on unix platforms). Then run the program again from the command-line
 with
-> python hammings.py
+> python infection_time.py
 This program is in python version 3, so make sure to use python 3.
 """
 import logging
@@ -60,16 +60,30 @@ def main():
         logging.info('Now working on %s.' % mpileup)
         identifier = read_identifier(mpileup)
         reference_genome = read_reference_genome(mpileup)
+        (pileup_df, empty_locations,
+         avg_depth, num_positions_covered) = parse_pileup(mpileup)
+
+        if len(empty_locations) != 0:
+            logging.debug('In %s, %d of the relevant locations had zero depth '
+                          '(%d were covered with %.2f average depth).'
+                          % (mpileup, len(empty_locations), num_positions_covered, avg_depth))
+
         try:
-            (average_hamming_distance, predicted_age,
-             avg_depth, num_positions_covered) = predict_age(mpileup)
+            average_hamming_distance, predicted_age = predict_age(pileup_df)
         except Exception as e:
             logging.error('File %s gave error: %s.' % (mpileup, e.args))
             continue
 
-        add_prediction_to_csv(identifier, reference_genome,
-                              average_hamming_distance, predicted_age,
-                              avg_depth, num_positions_covered)
+        try:
+            jc_time = calculate_jcdistance(pileup_df)
+        except Exception as e:
+            logging.error('File %s gave error: %s.' % (mpileup, e.args))
+            jc_time=0.0
+
+        add_prediction_to_csv(identifier=identifier, reference=reference_genome,
+                              avg_hamming=average_hamming_distance, age=predicted_age,
+                              avg_depth=avg_depth, num_pos_covered=num_positions_covered,
+                              jc_time=jc_time)
         logging.info('Predicted age %s from Hamming distance %.3f for identifier %s (reference %s).'
                      % (predicted_age, average_hamming_distance, identifier, reference_genome))
 
@@ -126,7 +140,7 @@ def read_reference_genome(mpileup_filename: str):
     reference = after_last_underscore.split('.')[0]
     return reference
 
-def predict_age(mpileup_filename: str):
+def predict_age(pileup_data: pd.DataFrame):
     """ Predicts age based on given mpileup file.
 
     Uses nucleotide frequency threshold hardcoded in this script.
@@ -134,18 +148,47 @@ def predict_age(mpileup_filename: str):
     :param mpileup_filename: string filename (without path).
     :return: float predicted age of infection in years.
     """
-    pileup_data, empty_locations = parse_pileup(mpileup_filename)
-    average_hamming = calculate_average_hamming_distance(pileup_data)
+    #pileup_data, empty_locations = parse_pileup(mpileup_filename)
+    average_hamming = calculate_average_pairwise_distance(pileup_data)
     predicted_age = round(average_hamming * SLOPE + INTERCEPT, 1)
-    avg_depth = pd.to_numeric(pileup_data.depth).mean()
-    num_positions_covered = len(pileup_data)
-
-    if len(empty_locations) != 0:
-        logging.debug('In %s, %d of the relevant locations had zero depth '
-                      '(%d were covered with %.2f average depth).'
-                      % (mpileup_filename, len(empty_locations), num_positions_covered, avg_depth))
     
-    return average_hamming, predicted_age, avg_depth, num_positions_covered
+    return average_hamming, predicted_age
+
+def calculate_jcdistance(pileup_df: pd.DataFrame) -> float:
+    """ Calculates Jukes-Cantor distance estimate of number of changes.
+
+    In the Jules-Cantor model, the frequency of mutants at a given location is
+    f = (3/4) - (3/4) * exp(-mu * t),
+    where mu is the rate of substituting one base for any other (including to its own, since all are equal).
+
+    Nowak 1999 suggests an error rate such as 10**(-4) per replication and Rodrigo et al (1999) argue that the
+    generation time is 1.2 days per generation. This means 10**(-4) / 1.2 errors per day, leading to mu such as
+    mu = (4/3) * 10**(-4) / 1.2
+    (since an error per time is (3/4) mu).
+
+    Cuevas et al 2015 find an overall mutation rate (in plasma genomes) of (9.3 +- 2.3) * 10**(-5) per base per cell.
+
+    Solving for the frequncy of mutatants at a given location gives the time in days
+    t = -(mu)**(-1) * ln(1 - (4/3)*f)
+
+    See https://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+
+    :param: pileup_df: pandas dataframe either with 'hamming' and 'reference' columns or which can be used in this
+    module's calculate_average_pairwise_distance().
+    :return: Jules-Cantor distance calculated using the hamming-based p-distance
+    """
+    pileup_df = pileup_df[pileup_df['reference'].isin(['A', 'C', 'T', 'G'])]
+    ref_freq = lambda row: row['%s_freq' % row['reference']]
+    pileup_df['ref_freq'] = pileup_df.apply(ref_freq, axis=1)
+
+    mu = (4 / 3) * 9.3 * 10**(-5) / 1.2
+    jc_t = lambda f: -(1/mu) * np.log(1 - (4 / 3) * f) / 365
+    jc_filter = lambda f: jc_t(f) if jc_t(f) < np.inf else np.nan
+    pileup_df['jc_t'] = pileup_df['ref_freq'].apply(jc_filter)
+
+    avg_jc_t = pileup_df['jc_t'].mean()
+
+    return avg_jc_t
 
 def verify_pileup_df(pileup_df: pd.DataFrame) -> None:
     """ #TODO: STUB! Verifies pileup dataframe and throws ValueError if errors are found. """
@@ -180,7 +223,8 @@ def parse_pileup(pileup_filename: str) -> (pd.DataFrame, pd.Series):
     """ Returns dataframe with frequencies from pileup file.
 
     :param pileup_filename: string filename.
-    :return: pandas dataframe.
+    :return: tuple of dataframe of locations, dataframe of empty locations, average depth, and number of positions
+    covered.
     """
 
     df = handheld_pileup_parsing(pileup_filename)
@@ -249,10 +293,18 @@ def parse_pileup(pileup_filename: str) -> (pd.DataFrame, pd.Series):
                       % (percent_with_wrong_frequency_sum, '%'))
 
     df = df.dropna()
-    return df, empty_locations
+    avg_depth = pd.to_numeric(df.depth).mean()
+    num_positions_covered = len(df)
 
-def calculate_average_hamming_distance(parsed_pileup_df: pd.DataFrame) -> float:
-    """ Predicts Hamming distance on dataframe from pileup file.
+    return df, empty_locations, avg_depth, num_positions_covered
+
+def calculate_average_pairwise_distance(parsed_pileup_df: pd.DataFrame) -> float:
+    """ Predicts average pairwise distance on dataframe from pileup file.
+
+    Using Puller, Neher Albert (2017) which says that this is the conventional Nei-Li nucleotide diversity.
+
+    In the pre-print, Puller, Neher and Albert (2017) called this the average hamming distance, so there was a bit of
+    confusion and the code still reflects that.
 
     :param parsed_pileup_df: pandas DataFrame with nucleotide frequencies.
     :return: float predicate age in years.
@@ -267,29 +319,43 @@ def calculate_average_hamming_distance(parsed_pileup_df: pd.DataFrame) -> float:
 
         heaviside_factor = 1 if THRESHOLD < (1 - x_m) else 0
 
-        hammond_distance = heaviside_factor * tofte_score_factor
+        hamming_distance = heaviside_factor * tofte_score_factor
 
-        return hammond_distance
+        return hamming_distance
 
-    parsed_pileup_df['hammonds'] = parsed_pileup_df.apply(calculate_summand, axis=1)
+    parsed_pileup_df['hamming'] = parsed_pileup_df.apply(calculate_summand, axis=1)
 
-    average_hammond_distance = sum(parsed_pileup_df['hammonds']) / len(parsed_pileup_df)
+    average_hamming_distance = sum(parsed_pileup_df['hamming']) / len(parsed_pileup_df)
 
-    return average_hammond_distance
+    return average_hamming_distance
 
-def add_prediction_to_csv(identifier: str, reference_genome: str,
-                          average_hamming_distance: float, predicted_age: float,
-                          avg_depth, num_positions_covered):
+def add_prediction_to_csv(*args, **kwargs):
     """ Adds prediction to predicted_ages.csv in same folder as this script.
 
     :param identifier: string identifier for mpileup file
     :param reference_genome: string name of reference genome for mpileup file.
     :return: None
+
+    identifier: str, reference_genome: str,
+                          average_hamming_distance: float, predicted_age: float,
+                          avg_depth, num_positions_covered,
+
     """
+    if kwargs:
+        assert not args, 'Do not give both key-worded values and values without keywords to be saved.'
+        cols = sorted(kwargs.keys())
+        values = [str(kwargs[key]) for key in cols]
+        if not Path(RESULTS_FILE).is_file():
+            with open(RESULTS_FILE, mode='w') as f:
+                f.write(','.join(cols) + '\n')
+    elif args:
+        values = [str(arg) for arg in args]
+    else:
+        raise ValueError('Either args or kwargs must be provided to add_prediction_to_csv.')
+
+    logging.info('Writing line to csv: %s' % ','.join(values))
     with open(RESULTS_FILE, mode='a') as f:
-        observation = ','.join([identifier, reference_genome,
-                                str(average_hamming_distance), str(predicted_age),
-                                str(avg_depth), str(num_positions_covered)])
+        observation = ','.join(values)
         f.write(observation + '\n')
 
     return None
